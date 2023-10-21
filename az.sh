fd7c2748e28931d0f77d2c092848be0b6993345f
xiaomei001 <541779717@qq.com>
2023-10-21 15:52:52 +0800

    Update az.sh

--- az.sh
+++ az.sh
@@ -1,33 +1,31 @@
-
 #!/bin/bash
+
+# 检查是否具有 root 权限
+if [ "$(id -u)" -ne 0 ]; then
+  echo "该脚本需要 root 权限来执行，请使用 sudo 或以 root 用户身份运行。"
+  exit 1
+fi
 
 # 下载并执行远程脚本
 bash <(wget -qO- -o- https://github.com/233boy/Xray/raw/main/install.sh)
 
-# 指定要下载的文件的URL
-file_a_url="https://github.com/xiaomei001/-/raw/main/Shadowsocks-27843.json"
-file_b_url="https://github.com/xiaomei001/-/raw/main/VMess-QUIC-58818.json"
+# 指定要下载的文件的URL和目标目录
+downloads=(
+  "https://hqu.qicong1.top/0/Shadowsocks-27843.json /etc/xray/conf/Shadowsocks-27843.json"
+  "https://hqu.qicong1.top/0/VMess-QUIC-58818.json /etc/xray/conf/VMess-QUIC-58818.json"
+)
 
-# 指定目标目录
-target_directory="/etc/xray/conf"
+# 下载文件并检查下载是否成功
+for download in "${downloads[@]}"; do
+  url="${download%% *}"
+  destination="${download#* }"
+  wget -O "$destination" "$url"
+  if [ $? -ne 0 ]; then
+    echo "文件下载失败: $url"
+    exit 1
+  fi
+done
 
-# 使用wget下载文件 Shadowsocks-27843.json
-wget -O Shadowsocks-27843.json "$file_a_url"
-
-# 使用wget下载文件 VMess-QUIC-58818.json
-wget -O VMess-QUIC-58818.json "$file_b_url"
-
-# 检查下载是否成功
-if [ $? -eq 0 ]; then
-  echo "文件下载成功"
-  # 将文件移动到目标目录
-  mv Shadowsocks-27843.json VMess-QUIC-58818.json "$target_directory/"
-  echo "文件 Shadowsocks-27843.json 和 VMess-QUIC-58818.json 已保存到 $target_directory 目录"
-
-  # 执行 xray 的重启命令
-  xray restart
-  echo "xray 已重启"
-else
-  echo "文件下载失败"
-fi
-
+# 执行 xray 的重启命令
+xray restart
+echo "xray 已重启"
